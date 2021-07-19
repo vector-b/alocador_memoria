f5e1251f3a186f67a9dd57d8a3e93b271d7dfd31
vgsb18 <vgsb18@inf.ufpr.br>
2021-07-19 15:41:19 -0300

    to ssh

--- pgma.c
+++ pgma.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-  void *a,*b;
+  void *a,*b,*c;
   iniciaAlocador ();
-  a = alocaMem (100) ;
+  a = alocaMem (200) ;
   b = alocaMem (200) ;
-  strcpy (a, " Preenchimento de Vetor ");
-  strcpy (b, a);
-  liberaMem (a);
-  liberaMem (b);
-  a = alocaMem(50);
+  c = alocaMem (200) ;
+  //imprimeMapa();
+  //strcpy (a, " Preenchimento de Vetor ");
+  //strcpy (b, a);
+  //liberaMem (a);
+  //liberaMem (b);
+  //a = alocaMem(50);
   //liberaMem(a);
   finalizaAlocador();
 }
